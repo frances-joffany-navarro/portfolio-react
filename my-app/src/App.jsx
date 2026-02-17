743dcd83f1a0dd42d96e87ecc38e611d918536ab
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2026-02-17 18:36:53 +0100

    Added css in carousel

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Header />
-      <Container className="main-container">
+      <Container className="main-container"> 
         <Hero />
       </Container>
       <Footer />
--- my-app/src/Hero.jsx
+++ my-app/src/Hero.jsx
@@ -1,41 +1,49 @@
 import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
 function IndividualIntervalsExample() {
-
+  let imgSrc = '../src/assets/carouselImages/';
+  let imgFormat = '.png';
   let heroData = [
     {
       id: 1,
-      title: 'First slide label',
+      title: 'Simbang Gabi Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageUrl: '../src/assets/carouselImages/Christmas-Booklet-2022.jpg',
+      imageName: 'design-booklet',
     },
     {
       id: 2,
-      title: 'Second slide label',
+      title: 'Simbang Gabi Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageUrl: '../src/assets/carouselImages/Christmas-Booklet-2025.png',
+      imageName: 'design-booklet-1',
     },
     {
       id: 3,
-      title: 'Third slide label',
+      title: 'Anniversary Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageUrl: '../src/assets/carouselImages/Christmas-Booklet-2025-v2.png',
+      imageName: 'design-booklet-2',
     },
   ];
 
   return (
-    <Carousel>
+    <Carousel fade pause='hover' variant='dark' >
       {
         heroData.map((slide) => {
           return (
 
-            <Carousel.Item key={slide.id} interval={1000}>
+            <Carousel.Item key={slide.id} interval={1500}>
               <img
-                className="d-block w-100"
-                src={slide.imageUrl}
+                className="d-none d-lg-block w-100"
+                src={imgSrc + slide.imageName + '-lg' + imgFormat}
                 alt={slide.title}
               />
-              <Carousel.Caption>
+              <img
+                className="d-block d-lg-none w-100"
+                src={imgSrc + slide.imageName + imgFormat}
+                alt={slide.title}
+              />
+              {/* <Image className="d-block w-100" src={slide.imageUrl} alt={slide.title} fluid />
+               */}<Carousel.Caption>
                 <h3>{slide.title}</h3>
                 <p>{slide.description}</p>
               </Carousel.Caption>
