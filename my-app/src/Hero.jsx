bdf4a0b43001982349cbe36e99df5f286b61796a
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2026-02-19 17:20:18 +0100

    update hero style

--- my-app/src/Hero.jsx
+++ my-app/src/Hero.jsx
@@ -1,6 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
 
+//images
+
 function IndividualIntervalsExample() {
   let imgSrc = '../src/assets/carouselImages/';
   let imgFormat = '.png';
@@ -31,22 +33,24 @@ function IndividualIntervalsExample() {
         heroData.map((slide) => {
           return (
 
-            <Carousel.Item key={slide.id} interval={1500}>
-              <img
+            <Carousel.Item key={slide.id} interval={3000}>
+              {/*  <img
                 className="d-none d-lg-block w-100"
                 src={imgSrc + slide.imageName + '-lg' + imgFormat}
                 alt={slide.title}
-              />
+              /> */}
+
+              <Image className="d-none d-md-block" src={imgSrc + slide.imageName + '-lg' + imgFormat} alt={slide.title} fluid />
+
               <img
-                className="d-block d-lg-none w-100"
+                className="d-block d-md-none center"
                 src={imgSrc + slide.imageName + imgFormat}
                 alt={slide.title}
               />
-              {/* <Image className="d-block w-100" src={slide.imageUrl} alt={slide.title} fluid />
-               */}<Carousel.Caption>
+              {/* <Carousel.Caption>
                 <h3>{slide.title}</h3>
                 <p>{slide.description}</p>
-              </Carousel.Caption>
+              </Carousel.Caption> */}
 
             </Carousel.Item>
 
