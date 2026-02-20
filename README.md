ef17e9b0b77c69e37af465c645fc26a083f47977
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2026-02-20 18:04:24 +0100

    deploy my app

--- README.md
+++ README.md
@@ -10,8 +10,10 @@ Project started 10 February 2026
  - [x] Create a style for the website. Should i use bootstrap?
  - [ ] Add content
    - [ ] Create Headline (Carousel of my works)
-     - [ ] fix the size of carousel
+     - [x] fix the size of carousel
        - [x] made the sizes of the images uniform
+       - [x] fix the streched image in responsive
+       - [ ] find good images for design and development categories
      - [ ] group my works by category (Development and Design)
      - [x] Fix the interval
    - [ ] Create My works section
--- my-app/src/Hero.jsx
+++ my-app/src/Hero.jsx
@@ -5,42 +5,36 @@ import Image from 'react-bootstrap/Image';
 
 function IndividualIntervalsExample() {
   let imgSrc = '../src/assets/carouselImages/';
-  let imgFormat = '.png';
+  let imgFormat = '.jpg';
   let heroData = [
     {
       id: 1,
       title: 'Simbang Gabi Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageName: 'design-booklet-4',
+      imageName: 'design',
     },
     {
       id: 2,
       title: 'Simbang Gabi Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageName: 'design-booklet-1',
-    },
-    {
-      id: 3,
-      title: 'Anniversary Booklet Design',
-      description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageName: 'design-booklet-2',
+      imageName: 'development',
     },
   ];
 
   return (
-    <Carousel fade pause='hover' variant='dark' >
+    <Carousel fade pause='hover' variant='dark'>
       {
         heroData.map((slide) => {
           return (
 
-            <Carousel.Item key={slide.id} interval={3000}>
+            <Carousel.Item key={slide.id} interval={5000}>
               {/*  <img
                 className="d-none d-lg-block w-100"
                 src={imgSrc + slide.imageName + '-lg' + imgFormat}
                 alt={slide.title}
               /> */}
 
-              <Image className="d-none d-md-block w-100" src={imgSrc + slide.imageName + '-lg' + imgFormat} alt={slide.title} fluid />
+              <Image className="d-none d-md-block w-100" src={imgSrc + slide.imageName + imgFormat} alt={slide.title} fluid />
               <Image className="d-block d-md-none w-100" src={imgSrc + slide.imageName + imgFormat} alt={slide.title} fluid />
               {/* <img
                 className="d-block d-md-none"
