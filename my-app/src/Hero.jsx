42b963baade3457849737b921a1d3f59957bf44e
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2026-02-20 18:40:05 +0100

    move images; update Hero component

--- my-app/src/Hero.jsx
+++ my-app/src/Hero.jsx
@@ -1,10 +1,9 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
-
+import imgSrc from 'my-app/src/assets/images/hero';
 //images
 
 function Hero() {
-  let imgSrc = 'carouselImages/';
   let imgFormat = '.jpg';
   let heroData = [
     {
@@ -28,23 +27,9 @@ function Hero() {
           return (
 
             <Carousel.Item key={slide.id} interval={5000}>
-              {/*  <img
-                className="d-none d-lg-block w-100"
-                src={imgSrc + slide.imageName + '-lg' + imgFormat}
-                alt={slide.title}
-              /> */}
 
               <Image className="d-none d-md-block w-100" src={imgSrc + slide.imageName + imgFormat} alt={slide.title} fluid />
               <Image className="d-block d-md-none w-100" src={imgSrc + slide.imageName + imgFormat} alt={slide.title} fluid />
-              {/* <img
-                className="d-block d-md-none"
-                src={imgSrc + slide.imageName + imgFormat}
-                alt={slide.title}
-              /> */}
-              {/* <Carousel.Caption>
-                <h3>{slide.title}</h3>
-                <p>{slide.description}</p>
-              </Carousel.Caption> */}
 
             </Carousel.Item>
 
