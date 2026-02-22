ca571525b7db90b8880ef253f640d0bdde6bd6aa
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2026-02-22 22:22:34 +0100

    update imagesrc

--- my-app/src/Hero.jsx
+++ my-app/src/Hero.jsx
@@ -1,22 +1,23 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
-import imgSrc from 'my-app/src/assets/images/hero';
+
 //images
 
 function Hero() {
-  let imgFormat = '.jpg';
+  //let imageFormat = '.jpg';
+  //const imageSrc = './portfolio-react/my-app/src/assets/images/hero/';
   let heroData = [
     {
       id: 1,
       title: 'Simbang Gabi Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageName: 'design',
+      imageSrc: 'portfolio-react/src/assets/images/hero/design.jpg',
     },
     {
       id: 2,
       title: 'Simbang Gabi Booklet Design',
       description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      imageName: 'development',
+      imageSrc: 'portfolio-react/src/assets/images/hero/development.jpg',
     },
   ];
 
@@ -28,8 +29,8 @@ function Hero() {
 
             <Carousel.Item key={slide.id} interval={5000}>
 
-              <Image className="d-none d-md-block w-100" src={imgSrc + slide.imageName + imgFormat} alt={slide.title} fluid />
-              <Image className="d-block d-md-none w-100" src={imgSrc + slide.imageName + imgFormat} alt={slide.title} fluid />
+              <Image className="d-none d-md-block w-100" src={slide.imageSrc} alt={slide.title} fluid />
+              <Image className="d-block d-md-none w-100" src={slide.imageSrc} alt={slide.title} fluid />
 
             </Carousel.Item>
 
